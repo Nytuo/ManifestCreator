05a9bc39613af7956c67f8da2f0a53a2d2f60467
Arnaud BEUX <34894524+Nytuo@users.noreply.github.com>
2021-02-28 12:13:43 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace ManifestCreatorC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGray;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(596, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGray;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(12, 366);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(557, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(29)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(578, 13);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Location of the working folder";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(29)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.ForeColor = System.Drawing.Color.White;
+            this.textBox2.HideSelection = false;
+            this.textBox2.Location = new System.Drawing.Point(12, 38);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(659, 322);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "Manifest will be wrote here so don\'t touch !";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.ForeColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(575, 366);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(96, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Install Manifest";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(29)))));
+            this.ClientSize = new System.Drawing.Size(683, 393);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Manifest-Creator  by Nytuo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManifestCreatorC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Clear();
+                textBox2.Clear();
+                string path = folderBrowserDialog1.SelectedPath;
+                textBox1.Text = path;
+                string directorymainname = Path.GetFullPath(folderBrowserDialog1.SelectedPath);
+                var directorylength = directorymainname.Length;
+                string[] allfiles = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+                int nbofrun = 0;
+                foreach (var file in allfiles)
+                {
+
+                    FileInfo info = new FileInfo(file);
+
+
+                    //DateTime modification = File.GetLastWriteTime(info.FullName);
+                    //CultureInfo culture = new CultureInfo("en-US");
+
+                    //DateTime modification = File.GetLastWriteTime(@"" + info.FullName);
+
+                    
+                    // Do something with the Folder or just add them to a list via nameoflist.add();
+                    
+                    var removebeforeroot = info.FullName;
+
+                    removebeforeroot = removebeforeroot.Substring((directorylength+1), removebeforeroot.Length - (directorylength+1));
+                    //textBox2.AppendText(removebeforeroot + " | " +modification.ToString("d",culture) + " | "+info.Length+Environment.NewLine);
+                    if (checkBox1.Checked == true)
+                    {
+                        if (nbofrun < allfiles.Length)
+                        {
+                            textBox2.AppendText(removebeforeroot +",0"+Environment.NewLine);
+                        }
+                        else
+                        {
+                            textBox2.AppendText(removebeforeroot + ",0" + info.Name);
+                        }
+                        nbofrun += 1;
+
+                    }
+                    else
+                    {
+                        if (nbofrun < allfiles.Length)
+                        {
+                            textBox2.AppendText(removebeforeroot + ",0," + info.Length+Environment.NewLine);
+                        }
+                        else
+                        {
+                            textBox2.AppendText(removebeforeroot + ",0," + info.Length);
+                        }
+                        nbofrun += 1;
+                    }
+                  
+                    
+
+
+                }
+                
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string directorymainname = Path.GetFileName(folderBrowserDialog1.SelectedPath);
+            saveFileDialog1.Filter = "txt files (*.txt*)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.Title = "save the manifest";
+            saveFileDialog1.FileName = directorymainname+" -- Manifest.txt";
+            if (saveFileDialog1.ShowDialog()== DialogResult.OK)
+            {
+                string path = saveFileDialog1.FileName;
+                StreamWriter sw = new StreamWriter(File.Create(path));
+                sw.Write(textBox2.Text);
+                sw.Dispose();
+            }
+        }
+    }
+}
